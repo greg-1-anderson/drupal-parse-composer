4ee1b9af8c7caad5da3e6049e51a6fbcde258833
Greg Anderson <greg.1.anderson@greeenknowe.org>
2015-02-03 17:23:40 -0800

    Yet another pointless commit.

--- src/Project.php
+++ src/Project.php
@@ -117,7 +117,7 @@ class Project
                     $composerMap[$top]['name'] = $this->getName();
                 }
                 $composerMap[$top]['type'] = 'drupal-drush';
-                // Require Drush 6 or later.  Because we want to.
+                // Require Drush 6 or later.  Because we want to.  And we should test it, too.
                 $composerMap[$top]['require']['drush/drush'] = '>=6';
             }
         }
